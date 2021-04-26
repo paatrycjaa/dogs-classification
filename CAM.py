798be30209206e737320a0d4fed2a4dbd3426ab5
PanAndrzej96 <andrzej.przybylski96@gmail.com>
2021-04-26 19:46:46 +0200

    Clear code for CAM

--- CAM.py
+++ CAM.py
@@ -7,80 +7,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from IPython.display import Image, display
+import math
 
+ALPHA_VAL = 0.0015
 
-def get_img_array(img_path, size):
-    # `img` is a PIL image of size 299x299
-    img = keras.preprocessing.image.load_img(img_path, target_size=size)
-    # `array` is a float32 Numpy array of shape (299, 299, 3)
-    array = keras.preprocessing.image.img_to_array(img)
-    # We add a dimension to transform our array into a "batch"
-    # of size (1, 299, 299, 3)
-    array = np.expand_dims(array, axis=0)
-    return array
+def make_heatmap_from_test_img(model, img_generator, layer_name, img_index):
+    numberOfBatches = math.ceil(img_index/img_generator.batch_size)
+    x, y = img_generator.test_array(numberOfBatches)
+    if (len(x)<img_index-1):
+        print("Image index too large")
+        return
 
+    img_array = keras.preprocessing.image.img_to_array(x[img_index])
+    img_array = np.expand_dims(img_array, axis=0)
+    heatmap = make_gradcam_heatmap(img_array, model.model, layer_name)
+    plt.matshow(heatmap)
+    plt.show()
+    save_and_display_gradcam(x[img_index], heatmap, alpha = ALPHA_VAL)
 
-def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
-    # First, we create a model that maps the input image to the activations
-    # of the last conv layer as well as the output predictions
+def make_gradcam_heatmap(img_array, model, layer_name, pred_index=None):
+    #mapping input and output of choosed layer
     grad_model = tf.keras.models.Model(
-        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+        [model.inputs], [model.get_layer(layer_name).output, model.output]
     )
-
-    # Then, we compute the gradient of the top predicted class for our input image
-    # with respect to the activations of the last conv layer
+    #compute gradient
     with tf.GradientTape() as tape:
-        last_conv_layer_output, preds = grad_model(img_array)
+        layer_output, preds = grad_model(img_array)
         if pred_index is None:
             pred_index = tf.argmax(preds[0])
         class_channel = preds[:, pred_index]
 
-    # This is the gradient of the output neuron (top predicted or chosen)
-    # with regard to the output feature map of the last conv layer
-    grads = tape.gradient(class_channel, last_conv_layer_output)
+    grads = tape.gradient(class_channel, layer_output)
 
-    # This is a vector where each entry is the mean intensity of the gradient
-    # over a specific feature map channel
     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
-
-    # We multiply each channel in the feature map array
-    # by "how important this channel is" with regard to the top predicted class
-    # then sum all the channels to obtain the heatmap class activation
-    last_conv_layer_output = last_conv_layer_output[0]
-    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    layer_output = layer_output[0]
+    heatmap = layer_output @ pooled_grads[..., tf.newaxis]
     heatmap = tf.squeeze(heatmap)
-
-    # For visualization purpose, we will also normalize the heatmap between 0 & 1
     heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
     return heatmap.numpy()
 
 def save_and_display_gradcam(img, heatmap, cam_path="cam.jpg", alpha=0.4):
-    # Load the original image
+    #preprocessing original image
     img = keras.preprocessing.image.img_to_array(img)
-
-    # Rescale heatmap to a range 0-255
+    #rescale heatmap to img size
     heatmap = np.uint8(255 * heatmap)
-
-    # Use jet colormap to colorize heatmap
+    #choose jet colormap
     jet = cm.get_cmap("jet")
-
-    # Use RGB values of the colormap
+    #Create image with based on RGB colorized heatmap
     jet_colors = jet(np.arange(256))[:, :3]
     jet_heatmap = jet_colors[heatmap]
-
-    # Create an image with RGB colorized heatmap
     jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
     jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
     jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
-
-    # Superimpose the heatmap on original image
+    #combine image with heatmap
     superimposed_img = jet_heatmap * alpha + img
     superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
 
-    # Save the superimposed image
     superimposed_img.save(cam_path)
-    # Display Grad CAM
     display(Image(cam_path))
-
-    return Image(cam_path)
+    return
 
